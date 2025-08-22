808e23611ca1a4ec9a23047b43a2d780d32885d3
Krenathakar <krenathakar1904@gmail.com>
2025-08-22 23:27:01 +0530

    Added new files

--- obj/Debug/net9.0/PMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/PMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2229b27be175393d588d59849618e08e6eebc575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ca7e992be8c234388c41a4a78acf66fc6317d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
